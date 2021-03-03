86c46de46de0da3db1e82c96e2521cc325475d8d
Ricardo M <ricardomongza@gmail.com>
2021-03-02 20:45:49 -0600

    Created main1.c includes static library

---
+++ src/main1.c
@@ -0,0 +1,22 @@
+// Uses static library - after compiling cannot change until compiling again
+
+#include <stdio.h>
+#include "../static/lib_distancias.a"   // static library
+
+int main(int argc, char *argv[]) {
+    float dlrecta;
+    int dhoriz, dvert;
+    	
+    dlrecta = dlr(3, 4, 10, 12);
+
+    printf("Distancia en línea recta: %d\n", dlrecta);
+
+	dhoriz = dh(3, 4, 10, 12 );
+	dvert = dv(3, 4, 10, 12);
+
+    printf("Distancia horizontal: %d\n", dhoriz);
+	printf("Distancia vertical: %d\n", dvert);
+
+	return 0;
+
+}
