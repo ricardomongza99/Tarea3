62d233a504d6ca501e554b45f270f7a021c10c7e
Ricardo M <ricardomongza@gmail.com>
2021-03-07 20:42:27 -0600

    Modified main.c files to include new xpm and ypm functions

--- src/main2.c
+++ src/main2.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[]) {
     float dlrecta;
-    int dhoriz, dvert;
+    int dhoriz, dvert, xpmedio, ypmedio;
     	
     dlrecta = dlr(3, 4, 10, 12);
 
@@ -16,6 +16,12 @@ int main(int argc, char *argv[]) {
 
     printf("Distancia horizontal: %d\n", dhoriz);
 	printf("Distancia vertical: %d\n", dvert);
+
+    xpmedio = xpm(2, 3, 12, 14);
+    ypmedio = ypm(2, 3, 12, 14);
+
+    printf("Punto medio en x: %d\n", xpmedio);
+	printf("Punto medio en y: %d\n", ypmedio);
 
 	return 0;
 }
